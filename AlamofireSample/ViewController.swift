ef5cca582459a3e7181e7b866e6618ae382ae636
dixon <dixon9197@gmail.com>
2016-07-07 10:15:07 +0800

    Initial Commit

---
+++ AlamofireSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlamofireSample
+//
+//  Created by dixon on 16/7/7.
+//  Copyright © 2016年 Monaco1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
